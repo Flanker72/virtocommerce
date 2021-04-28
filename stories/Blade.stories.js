74632ef83cb9db54b84855ecd907d89ca365b9cf
Yuri A Taranov <me@flanker72.ru>
2021-04-28 09:15:47 +0500

    Blade Storybook

---
+++ stories/Blade.stories.js
@@ -0,0 +1,55 @@
+/**
+ * Blade story (for demo).
+ * @author Yuri A Taranov <me@flanker72.ru>
+ */
+import VCBlade from "~/components/Blade";
+
+export default {
+   title: "Blade",
+   component: VCBlade,
+   argTypes: {
+      collapse: { action: "collapse", name: "collapse" },
+      expand: { action: "expand", name: "expand" }
+   }
+};
+
+const Template = (args, { argTypes }) => ({
+   components: { VCBlade },
+   props: Object.keys(argTypes),
+   template: '<vc-blade v-bind="$props" v-on="$props"></vc-blade>'
+});
+
+export const Blade = Template.bind({});
+Blade.args = {
+   icon: "star",
+   title: "My Awesome Blade",
+   subtitle:  "Optional Subtitle",
+   width: 500,
+   closable: false,
+
+   toolbarItems: [
+      { id: 1, icon: "sync-alt", title: "Refresh" },
+      { id: 2, icon: "plus", title: "Add" },
+      { id: 3, icon: "trash", title: "Delete", disabled: true },
+      { id: 4, icon: "download", title: "Import" },
+      { id: 5, icon: "upload", title: "Export" },
+      { id: 6, icon: "cut", title: "Cut", disabled: true },
+      { id: 7, icon: "paste", title: "Paste", disabled: true },
+      { id: 8, icon: "cubes", title: "Bulk export", disabled: true },
+    ],
+    breadcrumbs: [
+      {
+        id: 0,
+        title: "Back",
+        icon: "arrow-left",
+      },
+      {
+        id: 1,
+        title: "Electronics",
+      },
+      {
+        id: 2,
+        title: "Desktop",
+      },
+    ]
+};
